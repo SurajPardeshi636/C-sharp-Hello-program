8fde268db9dbcf4fadee9eff440f5189f85322d8
Suraj Rajendra Pardeshi <152958796+SurajPardeshi636@users.noreply.github.com>
2023-12-11 09:24:44 +0530

    Add files via upload
    This is the Hello program in C sharp

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi... this is m first C# Program");
+            Console.WriteLine("Hello EveryOne"); // this abovelineto usetio print the data in output screen
+
+            Console.ReadLine();// and this line to useto show thedata on screen as well as to hold the screen on OP data
+
+        }
+    }
+}
